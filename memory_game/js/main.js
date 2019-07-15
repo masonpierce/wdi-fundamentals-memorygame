0932300ddd56866e4242efdaa96154ed70d87635
Mason Pierce <masonpierce@Masons-MacBook-Air.local>
2019-07-15 13:57:09 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,18 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-
 console.log("User flipped" + cardOne);
 console.log("User flipped" + cardTwo);
 console.log("User flipped" + cardThree);
 console.log("User flipped" + cardFour);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = {};
+
+var cardOne = ["queen"];
+var cardTwo = {"king"};
+
+cardsInPlay.push("cardOne");
+cardsInPlay.push("cardTwo");
+
+console.log("User flipped queen")
+console.log("User flipped king")
